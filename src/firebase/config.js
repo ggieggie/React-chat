2f7e399610a3e66de909fb406aeee70fc27ccd3f
ggieggie <veggie.main@gmail.com>
2019-08-23 02:03:57 +0900

    add firebase chat

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDKEk0qrZQKIHhE0b-vBmhPbjL9DH-fUjo",
+    authDomain: "reactchat-9c2f2.firebaseapp.com",
+    databaseURL: "https://reactchat-9c2f2.firebaseio.com",
+    projectId: "reactchat-9c2f2",
+    storageBucket: "",
+    messagingSenderId: "427577593597",
+    appId: "1:427577593597:web:77971d4a650720a6"
+  };
+  
